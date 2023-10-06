8565fb8c8ab772f35f41920cf2bc374060f7d7b9
Phil Wilson <pipwilson@users.noreply.github.com>
2023-10-06 18:31:20 +0100

    Create index.php
    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Next bin day collection dates</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' media='all' />
+    </head>
+    <body class="container">
+        <h1>Next bin day collection dates</h1>
+<?php
+    $url = 'https://www.bathnes.gov.uk/webapi/api/BinsAPI/v2/getbartecroute/XXXXXXXXXXX/true';
+    $data = file_get_contents($url);
+    $json = json_decode($data, true);
+    echo("<p>Next black bin collection date: ".$json['residualNextFullDayandDateText']."</p>");
+    echo("<p>Next green bin collection date: ".$json['organicNextFullDayandDateText']."</p>");
+?>
+    </body>
+</html>
