2f4dce1ea0c6f837ebdab485691a708719a85a50
Phil Wilson <github@philwilson.org>
2024-04-17 13:00:02 +0100

    Switch to use a .env file

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    $env = parse_ini_file('.env');
+    $route_key = $env["COLLECTION_ROUTE_KEY"];
+?>
 <!doctype html>
 <html lang="en">
     <head>
@@ -8,9 +12,10 @@
     <body class="container">
         <h1>Next bin day collection dates</h1>
 <?php
-    $url = 'https://www.bathnes.gov.uk/webapi/api/BinsAPI/v2/getbartecroute/XXXXXXXXXXX/true';
+    $url = 'https://www.bathnes.gov.uk/webapi/api/BinsAPI/v2/getbartecroute/'.$route_key.'/true';
     $data = file_get_contents($url);
     $json = json_decode($data, true);
+    print_r($json);
     echo("<p>Next black bin collection date: ".$json['residualNextFullDayandDateText']."</p>");
     echo("<p>Next green bin collection date: ".$json['organicNextFullDayandDateText']."</p>");
 ?>
