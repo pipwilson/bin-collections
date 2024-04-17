2012f971783c67a56abb1b6d95246897fce2d432
Phil Wilson <github@philwilson.org>
2024-04-17 17:40:36 +0100

    Make it more obvious when bin week is by changing background colour etc

--- index.php
+++ index.php
@@ -1,23 +1,60 @@
 <?php
     $env = parse_ini_file('.env');
     $route_key = $env["COLLECTION_ROUTE_KEY"];
+    if (empty($route_key)) {
+        die("COLLECTION_ROUTE_KEY not set in .env file");
+    }
+
+    $url = 'https://www.bathnes.gov.uk/webapi/api/BinsAPI/v2/getbartecroute/'.$route_key.'/true';
+    $data = file_get_contents($url);
+    $json = json_decode($data, true);
+
+    $blackBinNextDate = new DateTime($json['residualNextDate']);
+    $greenBinNextDate = new DateTime($json['organicNextDate']);
+
+    $nextCollection = "";
+
+    if($blackBinNextDate < $greenBinNextDate) {
+        $nextCollection = "black";
+    } else {
+        $nextCollection = "green";
+    }
 ?>
 <!doctype html>
 <html lang="en">
     <head>
         <title>Next bin day collection dates</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' media='all' />
+        <style type="text/css">
+            body {
+                font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                margin: 0;
+                padding: 0;
+                margin: auto;
+            }
+            h1 {
+                margin: 0;
+                padding: 10px;
+                text-transform: uppercase;
+                font-size: 2.5em;
+            }
+            p {
+                margin: 0;
+                padding: 10px;
+            }
+            .black {
+                background-color: #000000;
+                color: #fff;
+            }
+            .green {
+                background-color: #74986d;
+                color: #000;
+            }
+        </style>
     </head>
-    <body class="container">
-        <h1>Next bin day collection dates</h1>
-<?php
-    $url = 'https://www.bathnes.gov.uk/webapi/api/BinsAPI/v2/getbartecroute/'.$route_key.'/true';
-    $data = file_get_contents($url);
-    $json = json_decode($data, true);
-    print_r($json);
-    echo("<p>Next black bin collection date: ".$json['residualNextFullDayandDateText']."</p>");
-    echo("<p>Next green bin collection date: ".$json['organicNextFullDayandDateText']."</p>");
-?>
+    <body class="<?= $nextCollection ?>">
+        <h1><?= $nextCollection ?> bin week</h1>
+        <p>Next black bin collection date: <?= $json['residualNextFullDayandDateText'] ?></p>
+        <p>Next green bin collection date: <?= $json['organicNextFullDayandDateText'] ?></p>
     </body>
 </html>
